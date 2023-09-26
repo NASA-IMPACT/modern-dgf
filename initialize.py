49baf3d44257253f79956e3b7f5d4df471c6c083
Ashish Acharya <ashish.acharya14@gmail.com>
2023-09-25 20:50:02 -0500

    Add detailed prompt

--- initialize.py
+++ initialize.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from rich.console import Console
 from rich.table import Table
+from rich.prompt import Prompt
+from rich import print
 
 import typer
 
@@ -37,6 +39,24 @@ def wizard():
         table.add_row(f"{index}. {entity}", *codes)
 
     console.print(table)
+    options_selected = Prompt.ask(
+        """Please enter codes for relevant documents to your project. All other documents will be deselected.
+
+Select all documents for all entities by typing in "all".
+
+Select all documents for an entity by typing in a whole number. For example, 1 will select all documents for the entity "Data".
+
+Select individual documents by typing in a decimal number. For example, 1.1 will select the first activity for the entity "Data".
+
+Select multiple documents by typing in a comma-separated list of numbers.
+
+Deselect a document by typing in a negative number. For example, -1 will deselect all documents for the entity "Data". -2.1 will Deselct the first document for the entity "Metadata".
+
+Example input: all
+Example input: 1, 2.3, 2.4, 2.5, -3.2, 6.1
+        """,
+    )
+    print(options_selected)
 
 
 if __name__ == "__main__":
