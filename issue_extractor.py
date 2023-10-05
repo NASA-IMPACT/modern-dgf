8dcbb7ad09080a76c1ec48b436de60a38c5c90e6
Ashish Acharya <ashish.acharya14@gmail.com>
2023-10-05 14:03:31 -0500

    Refactor issue extraction code

--- issue_extractor.py
+++ issue_extractor.py
@@ -7,6 +7,42 @@ import tqdm
 
 import github_helper
 
+
+def get_subfolders(folder):
+    """
+    Return the subfolders based on the given folder path.
+    """
+    subfolder_list = list(glob(folder + "*/*/", recursive=True))
+    if not subfolder_list:  # for resources/*/
+        subfolder_list = list(glob(folder + "*/", recursive=True))
+    return subfolder_list
+
+
+def extract_data_from_readme(subfolder):
+    """
+    Extract data from README.md within a given subfolder.
+    """
+    try:
+        df = pd.read_html(subfolder + "README.md")[0]
+    except ValueError:
+        print("No table found in " + subfolder + "README.md")
+        return None
+    return df
+
+
+def split_procedures_by_id(procedures):
+    """
+    Split procedures by ID and return a cleaned list.
+    """
+    split_by_id = re.split("(B\d.\d\.\d)", procedures)
+    split_by_id = [i for i in split_by_id if i]
+
+    procedures = [
+        split_by_id[i] + split_by_id[i + 1] for i in range(0, len(split_by_id), 2)
+    ]
+    return [i.strip() for i in procedures if i]
+
+
 folder_list = [
     "code",
     "data",
@@ -16,34 +52,20 @@ folder_list = [
     "resources/storage/",
 ]
 
-
 for folder in tqdm.tqdm(folder_list):
-    subfolder_list = list(glob(folder + "*/*/", recursive=True))
-    if not subfolder_list:  # for resources/*/
-        subfolder_list = list(glob(folder + "*/", recursive=True))
-    for subfolder in subfolder_list:
-        try:
-            df = pd.read_html(subfolder + "README.md")
-        except ValueError:
-            print("No table found in " + subfolder + "README.md")
+    for subfolder in get_subfolders(folder):
+        df = extract_data_from_readme(subfolder)
 
-        for row in df[0].iterrows():
-            description = row[1].iloc[0]
-            procedures = row[1].iloc[1]
+        if df is not None:
+            for _, row in df.iterrows():
+                description, procedures = row.iloc[0], row.iloc[1]
 
-            if isinstance(procedures, str):
-                split_by_id = re.split("(B\d.\d\.\d)", procedures)
-                split_by_id = [i for i in split_by_id if i]
-
-                procedures = [
-                    split_by_id[i] + split_by_id[i + 1]
-                    for i in range(0, len(split_by_id), 2)
-                ]
-                procedures = [i.strip() for i in procedures if i]
-                for procedure in procedures:
-                    github_helper.create_issues(
-                        "code-geek/dgf-playground",
-                        title=procedure,
-                        body=description,
-                    )
-                    time.sleep(5)
+                if isinstance(procedures, str):
+                    procedures_list = split_procedures_by_id(procedures)
+                    for procedure in procedures_list:
+                        github_helper.create_issues(
+                            "code-geek/dgf-playground",
+                            title=procedure,
+                            body=description,
+                        )
+                        time.sleep(5)
