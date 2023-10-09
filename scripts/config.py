960aca92388c332360c73ed82db979f8582e0949
xhagrg <grg.iksha@hotmail.com>
2023-10-09 13:26:09 -0500

    Add config file.

---
+++ scripts/config.py
@@ -0,0 +1,10 @@
+ENTITIES = ["Data", "Metadata", "Digital Content", "Code", "Storage", "People"]
+
+ENTITIY_FOLDERS = [
+    "data",
+    "metadata",
+    "digital_content",
+    "code",
+    "resources/storage",
+    "resources/people",
+]
